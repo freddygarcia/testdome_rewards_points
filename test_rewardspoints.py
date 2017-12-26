02b3e64ea79382c646c4fb0ee4551628c81c36b3
Freddy <buscando_alink@hotmail.com>
2017-12-26 00:10:44 -0400

    Improve tests

--- test_rewardspoints.py
+++ test_rewardspoints.py
@@ -1,8 +1,6 @@
 import unittest
 import rewardspoints
 
-
-
 class TestRewardPoints(unittest.TestCase):
 
 	def setUp(self):
@@ -10,28 +8,37 @@ class TestRewardPoints(unittest.TestCase):
 		self.rewardpoints.earn_points('Nick', 0)
 		self.rewardpoints.earn_points('John', 200)
 
-	def test_earn_points(self):
+	def test_receiving_bonus(self):
+		john_points = self.rewardpoints.consult_points('John')
+		self.assertEqual(john_points, 700)
 
+	def test_not_receiving_bonus(self):
+		nick_points = self.rewardpoints.consult_points('Nick')
+		self.assertEqual(nick_points, 0)
+
+	def test_earning_points_positive(self):
 		nick_points = self.rewardpoints.consult_points('Nick')
 		self.assertEqual(nick_points, 0)
 
 		john_points = self.rewardpoints.consult_points('John')
 		self.assertEqual(john_points, 700)
 
+	def test_not_earning_points_negative(self):
 		john_points = self.rewardpoints.earn_points('John', -5)
 		self.assertEqual(john_points, 700)
 
-	def test_spend_points(self):
-
-		nick_points = self.rewardpoints.spend_points('Nick', 100)
-		self.assertEqual(nick_points, 0)
-
+	def test_spend_points_less_than_zero(self):
 		nick_points = self.rewardpoints.spend_points('Nick', -500)
 		self.assertEqual(nick_points, 0)
 
 		john_points = self.rewardpoints.spend_points('John', -500)
 		self.assertEqual(john_points, 700)
 
+	def test_spend_points_greater_than_zero(self):
 		john_points = self.rewardpoints.spend_points('John', 20)
 		self.assertEqual(john_points, 680)
-		
+
+	def test_no_having_enough_points_to_spend(self):
+		nick_points = self.rewardpoints.spend_points('Nick', 100)
+		self.assertEqual(nick_points, 0)
+
